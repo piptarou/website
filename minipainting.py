a296b7ebf3812eb4aad4d9886274e23d8a774f6f
piptarou <piptarou@gmail.com>
2023-12-06 19:39:36 -0400

    First Python website commit

---
+++ minipainting.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Miniature Painting Portfolio")
+
+st.markdown("This is where I will showcase my models along with their medals if they received any.")
---
+++ pages/favourite_supplies.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("My favourite painting supplies")
+
+st.markdown("My favorite brushes as of 2023 are Rosemary & Co sable hair brushes. I enjoy using ProAcryl paints as they are generally smooth to use with not much thinning.")
